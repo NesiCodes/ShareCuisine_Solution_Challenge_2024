1a15ce45726188a7b371c48ee835818a79a161a9
Ziko Muca <128182963+zikomuca25@users.noreply.github.com>
2024-07-23 16:56:14 +0200

    Add files via upload

---
+++ lib/components/navbar_reciever_model.dart
@@ -0,0 +1,24 @@
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'navbar_reciever_widget.dart' show NavbarRecieverWidget;
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class NavbarRecieverModel extends FlutterFlowModel<NavbarRecieverWidget> {
+  /// Initialization and disposal methods.
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+
+  /// Action blocks are added here.
+
+  /// Additional helper methods are added here.
+}
